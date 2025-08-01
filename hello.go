3f265b4abe578e97de42d9652e8c2553e3c46ba4
LAPTOP-4COGLEIH\USER <yeesiew.socoe@gmail.com>
2025-08-01 15:25:52 +0800

    Test linting in Github Action go

--- hello.go
+++ hello.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Intentional unused variable
+	unused := "this is not used"
+
+	// Intentional shadowing of variable
+	msg := "Hello"
+	if true {
+		msg := "Shadowed Hello" // will shadow the outer msg
+		fmt.Println(msg)
+	}
+
+	// Calling a function with wrong argument
+	fmt.Println(strings.ToUpper(123)) // type error
+
+	// Unreachable code
+	return
+	fmt.Println("This will never run")
+
+	// Bad formatting / style
+fmt.Println("No indent")
 }
